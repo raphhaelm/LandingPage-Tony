ad20aaad830c6ba765ed83d42484c3555c474b4b
Raphael josé <32878457+raphael-jose@users.noreply.github.com>
2022-02-09 10:15:33 -0300

    atualização do background

--- js/particulaBackground.js
+++ js/particulaBackground.js
@@ -37,7 +37,7 @@ particlesJS("particles-container", {
       }
     },
     interactivity: {
-      detect_on: "canvas",
+      detect_on: "none",
       events: {
         onhover: { enable: true, mode: "repulse" },
         onclick: { enable: true, mode: "push" },
@@ -46,7 +46,7 @@ particlesJS("particles-container", {
       modes: {
         grab: { distance: 400, line_linked: { opacity: 1 } },
         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-        repulse: { distance: 200, duration: 0.4 },
+        repulse: { distance: 200, duration:105},
         push: { particles_nb: 4 },
         remove: { particles_nb: 2 }
       }
