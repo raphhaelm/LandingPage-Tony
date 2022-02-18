7e52ce4eea457de2753c98f872b90fedfb135c5c
raphhaelm <rmarquescomputadores@gmail.com>
2022-02-18 16:56:51 -0300

    Update JS : aprimorando recursos

---
+++ src/front/models/login.js
@@ -0,0 +1,13 @@
+class Users{
+    constructor(nome, sobrenome, email, senha, confirmaS, rg, cep, endereco){
+
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.email = email;
+    this.senha = senha;
+    this.confirmaS = confirmaS;
+    this.rg = rg;
+    this.cep = cep;
+    this.endereco = endereco;
+    }
+}
--- src/front/views/form.js
+++ src/front/views/form.js
@@ -1,23 +1,40 @@
+let email = 'emBranco';
+let passaword = 'emBranco';
+let emailRecupera = $("#inputEmailRec");
+
+// Função que completa os campos de endereço com requisição de CEP no API viaCep.
 $('#inputCEP').on('focusout', function () {
-    try {
-        $.ajax({
-            url: 'https://viacep.com.br/ws/' + $(this).val() + '/json/unicode/',
-            dataType: 'json',
-            success: function (resposta) {
-                $("#inputEndereco").val(resposta.logradouro);
-                $("#inputComplemento").val(resposta.complemento);
-                $("#inputBairro").val(resposta.bairro);
-                $("#inputCidade").val(resposta.localidade);
-                $("#inputUf").val(resposta.uf);
-                $("#inputNum").focus();
-            }
-        });
-    } catch (e) {
-        alert('Opsss... Preencha manualmente as informações')
+    if ($(this).val().length == 8) {
+        $("#input_validacao_form").css({ 'display': 'none' });
+        $("#input_validacao_form").css({ 'color': 'aqua' });
+        try {
+            $.ajax({
+                url: 'https://viacep.com.br/ws/' + $(this).val() + '/json/unicode/',
+                dataType: 'json',
+                success: function (resposta) {
+                    $("#inputEndereco").val(resposta.logradouro);
+                    $("#inputComplemento").val(resposta.complemento);
+                    $("#inputBairro").val(resposta.bairro);
+                    $("#inputCidade").val(resposta.localidade);
+                    $("#inputUf").val(resposta.uf);
+                    $("#inputNum").focus();
+                }
+            });
+
+        } catch (err) {
+            alert(this.val() + `é um CEP inválido`)
+        }
+    } else {
+        const cep = $("#inputCEP").val();
+        $("#input_validacao_form").val(cep + " não é um CEP válido!");
+        $("#input_validacao_form").css({ 'display': 'block' });
+        $("#input_validacao_form").css({ 'color': 'red' });
+        $("#inputCEP").val("")
+        $("#inputCEP").focus();
     }
+});
 
-})
-
+// Função que esconde os campos de login, para efetuar o cadastro .
 $('#cadastre').on('click', function () {
     $('.login').css({ 'display': 'none' })
     $('.form').css({ 'display': 'flex' })
@@ -25,6 +42,8 @@ $('#cadastre').on('click', function () {
     $('#logar').css({ 'border': 'none', 'background': 'rgba(255,255,255,0.1)', 'color': 'aqua' })
 
 });
+
+// Função que esconde os campos de cadastro, para efetuar o login.
 $('#logar').on('click', function () {
     $('.login').css({ 'display': 'flex' })
     $('.form').css({ 'display': 'none' })
@@ -32,45 +51,81 @@ $('#logar').on('click', function () {
     $('#cadastre').css({ 'border': 'none', 'background': 'rgba(255,255,255,0.1)', 'color': 'aqua' })
 });
 
-$('#btn-cadastro').on('click', function (e) {
-    e.preventDefault();
-    console.log('Olá Rapha');
-
-
-
-    $("#inputName").val("");
-    $("#inputSobre").val("");
-    $("#inputEmail").val("");
-    $("#inputSenha").val("");
-    $("#inputConfirma").val("");
-    $("#input_RG").val("");
-    $("#inputCEP").val("");
-    $("#inputEndereco").val("");
-    $("#inputComp").val("");
-    $("#inputNum").val("");
-    $("#inputPais").val("");
-    $("#inputBairro").val("");
-    $("#inputCidade").val("");
-    $("#inputUf").val("");
-})
-
+// Função que válida enail para recuperação de senha
 $('#btn-recuperar').on('click', function (e) {
     e.preventDefault();
-    let emailRec = $('#inputEmailRec');
-    if (emailRec.val().includes("@") && emailRec.val().includes('.com')) {
-        alert(`Operação realizada com sucesso. Digite o codigo recebido no seu email. `)
-
+    if (emailRecupera.val().includes("@", ".com") && emailRecupera.val().length > 6) {
+        alert(`Operação realizada com sucesso. Digite o codigo recebido no seu email. `);
+        $('#codigoRecupera').css({'display':'block'});
     } else {
         alert('Preencha o campo email, com uma conta válida!')
     }
+});
+
+//Função redireciona para pagina de login
+$('#codigoRecupera').on('focusout', function(){
+    window.location.href = './formularios.html';
 })
+
+// Função que válida email para login.
 $('#btn-enviar').on('click', function (e) {
     e.preventDefault();
     if ($('inputLogin').val().includes("@", ".com")) {
+        alert(`Operação realizada com sucesso.  `);
+    } else {
+        alert('Preencha o campo email, com uma conta válida!');
+    }
+});
 
-        alert(`Operação realizada com sucesso. Digite o codigo recebido no seu email. `)
+// Função que válida email de cadastro.
+$("#inputEmail").on('focusout', function (e) {
+    e.preventDefault();
+    if ($("#inputEmail").val().includes('@', '.com')) {
+        email = 'emailValido';
+        $("#inputEmail").css({ 'background-color': "rgb(156, 230, 95, 0.9)" })
+    } else {
+        $("#inputEmail").css({ 'background-color': "#ff00005d" })
+    }
+});
+
+//Função que confere se senha e confirmação são iguais;
+$("#inputConfirma").on('focusout', function (e) {
+    e.preventDefault();
+    let senha = $('#inputSenha').val();
+    let confirmacao = $("#inputConfirma").val();
+    if (senha == confirmacao) {
+        $("#inputSenha").css({ 'background-color': "rgb(156, 230, 95, 0.9)" });
+        $("#inputConfirma").css({ 'background-color': "rgb(156, 230, 95, 0.9)" });
+        passaword = 'senhaValida';
+    } else {
+        $("#inputSenha").css({ 'background-color': "#ff00005d" });
+        $("#inputConfirma").css({ 'background-color': "#ff00005d" });
+
+    }
+});
+
+//Função que válida o cadastro.
+$('#btn-cadastro').on('click', function (e) {
+    e.preventDefault();
+    if (email == 'emailValido' && passaword == 'senhaValida' && $("#inputName").val() != "" && $("#inputSobre").val() != "") {
+        $("#input_validacao_form").css({ 'display': 'block' });
+        $("#input_validacao_form").val(`Cadastro efetuado com sucesso.`);
+        alert(` Cadastro efetuado com sucesso.
+        Efetuei login para acompanhar o desenvolvimento de TONY`);
+        $('.login').css({ 'display': 'flex' })
+        $('.form').css({ 'display': 'none' })
+        $('#logar').css({ 'border': '#5819ac 2px solid', 'background': 'rgba(255,255,255,0.8)', 'color': 'aqua' })
+        $('#cadastre').css({ 'border': 'none', 'background': 'rgba(255,255,255,0.1)', 'color': 'aqua' })
 
     } else {
-        alert('Preencha o campo email, com uma conta válida!')
+        alert('Dados incompletos! Preencha todos os campos para cadastro!');
+        $("#inputName").css({'border': 'red solid 2px'});
+        $("#inputSobre").css({'border': 'red solid 2px'});
+        $("#inputEmail").css({'border': 'red solid 2px'});
+        $("#inputSenha").css({'border': 'red solid 2px'});
+        $("#inputConfirma").css({'border': 'red solid 2px'});
+        $("#input_RG").css({'border': 'red solid 2px'}); 
+        $("#inputCEP").css({'border': 'red solid 2px'});
+        $("#inputNum").css({'border': 'red solid 2px'});
     }
 })
